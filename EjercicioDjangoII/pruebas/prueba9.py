82d4f1c33c447f503cd97845091ea78f3cc6d8e0
fraferang <francisco.fer.an@gmail.com>
2021-01-15 01:00:19 +0100

    Modificado css de menu de inicio y de mostrar los albumes
    en la prueba buscaba solo el 1 div, ahora busca todos pero no consigo sacar datos, mañana sigo con ello

--- EjercicioDjangoII/pruebas/prueba9.py
+++ EjercicioDjangoII/pruebas/prueba9.py
@@ -28,12 +28,14 @@ from datetime import datetime
 def recibir_datos():
     f = urllib.request.urlopen("https://www.eventbrite.es/d/spain/music--events/?page=1")
     s = BeautifulSoup(f, "lxml")
-    lista_albumes = s.find("ul", class_="search-main-content__events-list").find_all("div")
+    lista_albumes = s.find("ul", class_="search-main-content__events-list").find_all("li")
     for link_album in lista_albumes:
         f = urllib.request.urlopen(link_album.a['href'])
         s = BeautifulSoup(f, "lxml")
         datos = s.find("div", class_="g-group")
         print(datos)
+        concierto = s.find("a", class_="eds-event-card-content__action-link")
+        print(concierto)
     
         
         
