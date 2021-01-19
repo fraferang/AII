55a1843289c8003a68c3841df4544423f3f1bccc
fraferang <francisco.fer.an@gmail.com>
2021-01-19 10:26:05 +0100

    No saco el datos

--- EjercicioDjangoII/pruebas/prueba9.py
+++ EjercicioDjangoII/pruebas/prueba9.py
@@ -23,21 +23,31 @@ from datetime import datetime
 
 
 
-
-
 def recibir_datos():
-    f = urllib.request.urlopen("https://www.eventbrite.es/d/spain/music--events/?page=1")
+    f = urllib.request.urlopen("https://entradas.correos.es/categoria/conciertos-98?page=1")
     s = BeautifulSoup(f, "lxml")
-    lista_albumes = s.find("ul", class_="search-main-content__events-list").find_all("li")
+    lista_albumes = s.find("div", class_="package-list-item-wrapper").find_all("div")
     for link_album in lista_albumes:
-        f = urllib.request.urlopen(link_album.a['href'])
+        f = urllib.request.urlopen(link_album.a['href'])    
         s = BeautifulSoup(f, "lxml")
-        datos = s.find("div", class_="g-group")
-        print(datos)
-        concierto = s.find("a", class_="eds-event-card-content__action-link")
-        print(concierto)
-    
-        
+        datos = s.find("div", class_="package-list-item")
+        concierto =datos.find("div", class_="content").find("h4", class_="mb-0").find("a").string
+        Lugar = datos.find("ul", class_="list-info").find("span" , class_="font600").string
+#         concierto = datos.find("div", class_="eds-text-color--primary-brand eds-l-pad-bot-1 eds-text-weight--heavy eds-text-bs")
+#         print(concierto)
+
+# def recibir_datos():
+#     f = urllib.request.urlopen("https://www.eventbrite.es/d/spain/music--events/?page=1")
+#     s = BeautifulSoup(f, "lxml")
+#     lista_albumes = s.find("ul", class_="search-main-content__events-list").find_all("li")
+#     for link_album in lista_albumes:
+#         f = urllib.request.urlopen(link_album.a['href'])
+#         s = BeautifulSoup(f, "lxml")
+#         datos = s.find("div", class_="g-group")
+#         print(datos)
+#         concierto = datos.find("div", class_="eds-text-color--primary-brand eds-l-pad-bot-1 eds-text-weight--heavy eds-text-bs")
+#         print(concierto)
+   
         
         
 
